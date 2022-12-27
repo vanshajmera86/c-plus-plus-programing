a3f98b7c2114ee17ba31af48f75f86da18289bef
vanshajmera86 <121329493+vanshajmera86@users.noreply.github.com>
2022-12-27 23:18:29 +0530

    Create classobject.cpp

---
+++ classwork/classobject.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+/*
+	OOP : Object Oriented Programming.
+	
+	Features of OOP
+	
+	1) Encapsulation
+	2) Inheritance
+	3) Data Abstraction
+	4) Polymorphism
+	
+	
+	1) Encapsulation : It is another name of class.
+	
+	class : it is a collection of Data Members(variables)
+	        and Member Functions. (functions)
+	        
+	object : It is a real world entity.
+	
+		Lifecycle of an object
+		
+		1) Creation.
+		2) Utilization.
+		3) Destruction.
+	        
+	e.g Paper cup
+	
+		1) create
+		2) use
+		3) destroy
+		
+	
+
+*/
+
+
+
+
+int main()
+{
+	return 0;
+}
