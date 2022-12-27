b8997cdfe7ba1ddbb30208c45193ef336dd31564
vanshajmera86 <121329493+vanshajmera86@users.noreply.github.com>
2022-12-27 18:26:09 +0530

    Create test1.cpp

---
+++ classwork/test1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+/*
+	C++ -> C + Class
+*/
+
+int main()
+{
+	cout<<"Welcome to C++ Programming"<<endl;
+	cout<<"In Tops Technology."<<endl;
+	int a=43,b=64;
+	//cout<<"A = "<<a;
+	//cout<<"\nB = "<<b;
+	cout<<"A = "<<a<<"\nB = "<<b;
+	
+	return 0;
+}
