68653f4d34db32601e26a2419ab39fab860c8456
vanshajmera86 <121329493+vanshajmera86@users.noreply.github.com>
2023-01-10 14:44:36 +0530

    Create static demo.cpp

---
+++ labwork/static demo.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class staticdemo
+{
+	public:
+		static int a;
+		
+		void setval()
+		{
+			cout<<"\n\n enter a :";
+			cin>>a;
+		}
+		
+		void putval()
+		{
+			cout<<"\n a="<<a;
+		}
+		
+		
+};
+
+int staticdemo::a=0;
+
+
+
+
+
+int main()
+{
+	staticdemo d1,d2,d3;
+	d1.setval();
+	d2.setval();
+	d3.setval();
+	
+	d1.putval();
+	d2.putval();
+	d3.putval();
+	return 0;
+}
